56111321bcee062858d633ffb8de1280c405c6d0
nfebrian13 <nana.febriana@sinarmasmsiglife.co.id>
2019-12-23 15:32:54 +0700

    rest template

--- springboot-beginner/src/main/java/com/beginner/apps/SpringbootBeginnerApplication.java
+++ springboot-beginner/src/main/java/com/beginner/apps/SpringbootBeginnerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringbootBeginnerApplication implements ApplicationRunner {
@@ -20,5 +22,14 @@ public class SpringbootBeginnerApplication implements ApplicationRunner {
 	public void run(ApplicationArguments arg0) throws Exception {
 		System.out.println("Hello World from Application Runner");
 	}
-	
+
+	/*
+	 * #Tutorialspoint - Rest Template
+	 * 
+	 */
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
 }
